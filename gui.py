b99496840f5f4b270e5df7addbc6b4d579eb7c87
Muhammad Afaq <muhammadafaq1999@gmail.com>
2024-03-25 03:40:32 +0400

    Implement the Complete and Exit Buttons, day 18

--- gui.py
+++ gui.py
@@ -7,13 +7,16 @@ add_button = sg.Button("Add")
 list_box = sg.Listbox(values=functions.get_todos(), key='todos',  # use different keys
                       enable_events=True, size=(45, 10))
 edit_button = sg.Button("Edit")
-
+complete_button = sg.Button("Complete")
+exit_button = sg.Button("Exit")
 # layout expects a list of list and each of the list consists of PySimpleGUI object instances (widget)
 # think of each row as a separate list the overall list
 # window = sg.Window("My To-Do App", layout=[[label, input_box]])  # same line
 window = sg.Window("My To-Do App",
-                   layout=[[label], [input_box, add_button],
-                           [list_box, edit_button]],
+                   layout=[[label],
+                           [input_box, add_button],
+                           [list_box, edit_button, complete_button],
+                           [exit_button]],
                    font=('Helvetica', 20))
 
 while True:
@@ -35,9 +38,19 @@ while True:
             todos[index] = new_todo + '\n'
             functions.write_todos(todos)
             window['todos'].update(values=todos)
+        case "Complete":
+            todo_to_complete = values['todos'][0]
+            todos = functions.get_todos()
+            todos.remove(todo_to_complete)
+            functions.write_todos(todos)
+            window['todos'].update(values=todos)
+            window['todo'].update(value="")  # to remove completed task from input box
+        case "Exit":
+            break
         case 'todos':
             window['todo'].update(value=values['todos'][0])
         case sg.WINDOW_CLOSED:
+            # 'break' stops the loop where as 'exit()' stops the program
             break
 
 
